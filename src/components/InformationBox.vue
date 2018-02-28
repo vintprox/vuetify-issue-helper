84af6ba419ecff2648e6d49357c8dd4764b7d63e
Brandon Deo <bdeo@udel.edu>
2018-02-27 23:01:47 -0500

    Adds clarity to information section

--- src/components/InformationBox.vue
+++ src/components/InformationBox.vue
@@ -3,7 +3,7 @@
     <v-card-title><span class="title">Information</span></v-card-title>
     <v-card-text>
       <p>
-        This form is for submitting new issues to the <a href="https://www.github.com/vuetifyjs/vuetify" target="_blank">Vuetify</a> repository. If you're just looking for help, ask the community instead. We're super helpful, come <a href="https://chat.vuetifyjs.com">chat</a> with us!
+        This form is for submitting new issues to <a href="https://www.github.com/vuetifyjs" target="_blank">Vuetifyjs</a> repositories. If you're just looking for help, try asking the <a href="https://community.vuetifyjs.com" target="_blank" >community</a>!
       </p>
 
       <p>
@@ -11,19 +11,19 @@
       </p>
 
       Before submitting an issue:
-      <ol>
-        <li>Create a minimal reproduction to illustrate the issue. Here's a <a href="https://template.vuetifyjs.com">CodePen template</a> to start you off.
-          <ul>
-            <li>If you need a full project to reproduce, consider <a href="https://codesandbox.io/s/vue">CodeSandbox</a></li>
-          </ul>
-        </li>
+      <ul>
         <li>Search the repo and ensure your issue doesn't already exist. If so, it may:
           <ul>
             <li>Have already been fixed in an unreleased version.</li>
             <li>Have been closed without a solution. Please create a new issue instead of commenting on the old one.</li>
           </ul>
         </li>
-      </ol>
+        <li>Create a minimal reproduction to illustrate the issue. Here's a <a href="https://template.vuetifyjs.com">CodePen template</a> to start you off.
+          <ul>
+            <li>If you need a full project to reproduce, consider <a href="https://codesandbox.io/s/vue">CodeSandbox</a></li>
+          </ul>
+        </li>
+      </ul>
     </v-card-text>
   </v-card>
 </template>
