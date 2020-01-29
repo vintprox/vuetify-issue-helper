33952c604a2140fa54b268048cc6f1738a373438
John Leider <john.j.leider@gmail.com>
2020-01-29 01:14:07 -0500

    chore: restructure app, clean-up process

---
+++ src/layouts/base/AppBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar
+    app
+    color="white"
+    elevate-on-scroll
+  >
+    <v-responsive
+      class="mx-auto px-3"
+      max-width="800"
+    >
+      <div class="d-flex align-center">
+        <v-img
+          :src="require('@/assets/v.png')"
+          class="mr-3"
+          contain
+          max-width="40"
+        />
+
+        <v-toolbar-title class="font-weight-light">
+          Vuetify Issue Creator
+        </v-toolbar-title>
+
+        <v-spacer />
+
+        <v-btn
+          color="primary"
+          depressed
+          href="https://vuetifyjs.com/?ref=issues.vuetifyjs.com"
+          target="_blank"
+        >
+          Documentation
+
+          <v-icon right>
+            mdi-open-in-new
+          </v-icon>
+        </v-btn>
+      </div>
+    </v-responsive>
+  </v-app-bar>
+</template>
+
+<script>
+  export default { name: 'BaseAppBar' }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,33 @@
+// Imports
 import Vue from 'vue'
 import Router from 'vue-router'
-import { routes } from './routes'
 
 Vue.use(Router)
 
 export default new Router({
-  routes,
   mode: 'history',
-  scrollBehavior (to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    }
-    if (to.hash) {
-      return { selector: to.hash }
-    }
+  base: process.env.BASE_URL,
+  scrollBehavior: (to, from, savedPosition) => {
+    if (to.hash) return { selector: to.hash }
+    if (savedPosition) return savedPosition
+
     return { x: 0, y: 0 }
   },
+  routes: [
+    {
+      path: '',
+      component: () => import('@/layouts/base/Index'),
+      children: [
+        {
+          path: '/',
+          name: 'IssueCreator',
+          component: () => import('@/components/IssueCreator.vue'),
+        },
+        {
+          path: '*',
+          component: () => import('@/components/404.vue'),
+        },
+      ],
+    },
+  ],
 })
