b84d666863421822b0dda71dff870feb6b5cfd05
John Leider <john.j.leider@gmail.com>
2020-01-29 15:22:47 -0500

    refactor(InformationBox): improve code style

--- src/components/InformationBox.vue
+++ src/components/InformationBox.vue
@@ -1,10 +1,11 @@
 <template>
   <v-card class="grey lighten-4">
-    <v-card-title><span class="title">Information</span></v-card-title>
+    <v-card-title>Information</v-card-title>
+
     <v-card-text>
       <p>
         This form is for submitting new issues to <a
-          href="https://www.github.com/vuetifyjs"
+          href="https://www.github.com/vuetifyjs?ref=issues.vuetifyjs.com"
           target="_blank"
         >Vuetifyjs</a> repositories. If you're just looking for help, try asking the <a
           href="https://community.vuetifyjs.com"
@@ -22,9 +23,11 @@
           Search the repo and ensure your issue doesn't already exist. If so, it may:
           <ul>
             <li>Have already been fixed in an unreleased version.</li>
+
             <li>Have been closed without a solution. Please create a new issue instead of commenting on the old one.</li>
           </ul>
         </li>
+
         <li>
           Create a minimal reproduction to illustrate the issue. Here's a <a
             href="https://template.vuetifyjs.com"
@@ -37,6 +40,7 @@
                 target="_blank"
               >CodeSandbox</a>
             </li>
+
             <li>
               If you need a reproduction with router, consider <a
                 href="https://codepen.io/johnjleider/pen/PMZvpr"
